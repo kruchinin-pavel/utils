4d91e26782b563d9d93844a25ae1da12a659e1bb
kruchinin-pavel <kruchinin.1977@ya.ru>
2018-03-21 15:07:07 +0300

    remove key from git

--- src/test/java/org/kpa/util/telegram/TelegramBotTest.java
+++ src/test/java/org/kpa/util/telegram/TelegramBotTest.java
@@ -3,7 +3,7 @@ package org.kpa.util.telegram;
 public class TelegramBotTest {
 
     public static void main(String[] args) throws InterruptedException {
-        TelegramBot bot = TelegramBot.mySuperBot("chats.json");
+        TelegramBot bot = TelegramBot.get("MyExtraSuperBot", "", "chats.json");
         bot.broadcast("Hi from test!", true);
         Thread.sleep(10000);
     }
